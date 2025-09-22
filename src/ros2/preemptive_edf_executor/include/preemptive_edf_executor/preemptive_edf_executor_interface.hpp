746c2cfb5e078eb5cf684b7f6d9b60b618ed1468
Rocksen1683 <edpravee@uwaterloo.ca>
2025-09-22 12:17:02 -0400

    set callback details impl

--- src/ros2/preemptive_edf_executor/include/preemptive_edf_executor/preemptive_edf_executor_interface.hpp
+++ src/ros2/preemptive_edf_executor/include/preemptive_edf_executor/preemptive_edf_executor_interface.hpp
@@ -33,38 +33,11 @@ class PreemptiveEDFExecutorInterface : rclcpp::memory_strategy::MemoryStrategy
 {
 public:
     //constructor
-    explicit PreemptiveEDFExecutorInterface(std::shared_ptr<Alloc> allocator);
-    PreemptiveEDFExecutorInterface();
+    explicit PreemptiveEDFExecutorInterface(std::shared_ptr<Alloc> allocator) : allocator_(allocator) {}
+    PreemptiveEDFExecutorInterface() : allocator_(make_shared<Alloc>()) {}
 
-    //overrides from MemoryStrategy base class
-    void add_guard_condition(const rclcpp::GuardCondition & guard_condition) override;
-    void remove_guard_condition(const rclcpp::GuardCondition * guard_condition) override;
-    void clear_handles() override;
-    void remove_null_handles(rcl_wait_set_t *wait_set) override;
-    bool collect_entities(const WeakNodeList &weak_nodes) override;
-    void add_waitable_handle(const rclcpp::Waitable::SharedPtr &waitable) override;
-    bool add_handles_to_wait_set(rcl_wait_set_t *wait_set) override;
+    void set_callback_details(std::shared_ptr<const void> callback, ExecutableType type, int deadline, int period, int runtime, int chain_id); 
 
-    // commented some stuff out as it is undecided 
-
-    /*
-    // executable retrieval methods
-    void get_next_executable(rclcpp::AnyExecutable &any_exec, const WeakNodeList &weak_nodes) override;
-    void get_next_subscription(rclcpp::AnyExecutable &any_exec, const WeakNodeList &weak_nodes) override;
-    void get_next_service(rclcpp::AnyExecutable &any_exec, const WeakNodeList &weak_nodes) override;
-    void get_next_client(rclcpp::AnyExecutable &any_exec, const WeakNodeList &weak_nodes) override;
-    void get_next_timer(rclcpp::AnyExecutable &any_exec, const WeakNodeList &weak_nodes) override;
-    void get_next_waitable(rclcpp::AnyExecutable &any_exec, const WeakNodeList &weak_nodes) override;
-
-    // EDF specific methods
-    void post_execute(rclcpp::AnyExecutable any_exec, int thread_id = -1);
-    void set_executable_deadline(std::shared_ptr<const void> handle, int deadline, int period, ExecutableType t, int chain_id);
-    
-    // chain management methods
-    void add_callback_to_chain(std::shared_ptr<const void> callback, int chain_id);
-    std::vector<int> get_chains_for_callback(std::shared_ptr<const void> callback);
-    std::vector<PreemptiveEDFExecutable> get_executables_in_chain(int chain_id);
-    */
 
 private:
     std::shared_ptr<VoidAlloc> allocator_;
@@ -83,6 +56,7 @@ private:
     std::vector<const rclcpp::GuardCondition *> guard_conditions_;
 
     // helper methods => SUBJECT TO CHANGE
+    void store_handle(std::shared_ptr<const void> handle, ExecutableType type);     
     std::shared_ptr<PreemptiveEDFExecutable> get_and_reset_executable(std::shared_ptr<const void> executable, ExecutableType t);
     void add_executable_to_ready_queue(std::shared_ptr<PreemptiveEDFExecutable> executable);
 };
